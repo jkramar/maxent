a17c9f6df96fb5ea7dd12b8ed49cf545f7707cd1
Viliam Simko <viliam.simko@gmail.com>
2016-03-29 11:58:08 +0200

    added useDynLib to NAMESPACE instead of library.dynam

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,13 @@
 .onLoad <- function(libname, pkgname) {
-  library.dynam("maxent", pkgname, libname)
-  assign("maximumentropy", Module("maximumentropy", PACKAGE = "maxent"),
+  assign("maximumentropy",
+         Module("maximumentropy", PACKAGE = "maxent"),
          envir = .BaseNamespaceEnv)
   setClass("maxent", representation(model = "character",
                                     weights = "data.frame"))
+}
+
+.onAttach <- function(libname, pkgname) {
+  # just to show a startup message
+  message <- paste(pkgname, utils::packageVersion(pkgname), "loaded.")
+  packageStartupMessage(message, appendLF = TRUE)
 }
